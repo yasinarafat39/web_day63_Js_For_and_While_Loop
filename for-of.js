5f464f660a9429894ddc98971206c0d1c552432c
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-01 22:22:38 +0600

    for of loop practice

---
+++ for-of.js
@@ -0,0 +1,7 @@
+const numbers = [12, 98, 45 ,15, 64, 74, 42, 63];
+
+console.log(numbers);
+
+for(let num of numbers){
+    console.log(num);
+}
---
+++ recap.js
@@ -0,0 +1,16 @@
+// const: value will not change
+// var: we should not use it
+
+// variable
+const amount = 200;
+
+//  condition
+if(amount > 100){
+
+} else{
+
+}
+
+
+// array
+const numbers = [12, 32, 54, 65];
