dde0effaf3af748e2d216f5d0d62ef8f3ace6951
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-01 22:49:29 +0600

    while loop practice

---
+++ while.js
@@ -0,0 +1,17 @@
+
+
+const number = 7;
+if (number < 1){
+    console.log('choto ekta number paisi')
+}
+
+console.log('something new is cooking')
+
+
+//________________ WHILE LOOP ____________________
+
+let num = 0; // loop variable
+while(num < 5){
+    console.log(`looooooooping`, num);
+    num++;
+}
